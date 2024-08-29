94878669348c2de1c35f8523a8499287fe8ece57
Owen Murcia <owensebas2013@gmail.com>
2024-08-28 19:52:09 -0500

    V10.2
    Ya se pueden listar tareas (1:35:34)

--- tasks/views.py
+++ tasks/views.py
@@ -65,13 +65,19 @@ def signin(request):
 
 #Para crear tareas
 def create_task(request):
-
     if request.method == 'GET':
         return render(request, 'create_task.html',{
             'form': TaskForm
         })
     else:
-        print(request.POST)
-        return render(request, 'create_task.html',{
-            'form': TaskForm
-        })
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html',{
+                'form': TaskForm,
+                'error': 'No se pudo crear la tarea por favor ingresa datos validos'
+            })
