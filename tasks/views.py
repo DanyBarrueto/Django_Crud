49bb31acae6aa1b9826c2240a1589767148baa7e
Avilita10 <avilita994@gmail.com>
2024-08-29 00:00:44 -0500

    v12
    2:13:03
    Se puso opcion de eliminar, lista de tareas completadas, fecha y hora en las tareas completadas

--- tasks/views.py
+++ tasks/views.py
@@ -106,3 +106,13 @@ def complete_task(request, task_id):
         task.datecompleted = timezone.now()
         task.save()
         return redirect('tasks')
+    
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html',{'tasks': tasks} )
